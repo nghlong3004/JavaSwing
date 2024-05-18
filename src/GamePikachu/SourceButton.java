efc8cadb4c27dae207bd1fe4f8a647a27cc50a62
nghlong3004 <nghlong3004@gmail.com>
2024-05-18 17:34:18 +0700

    update

--- src/GamePikachu/SourceButton.java
+++ src/GamePikachu/SourceButton.java
@@ -1,35 +1,40 @@
 package GamePikachu;
 
+
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class SourceButton implements ActionListener{
-	private JButton[][] button;
+	private final static String address = "C:\\Users\\hh1305\\LONG\\JAVA\\JavaSwing\\src\\GamePikachu\\Image\\";
+	private final static int colSize = 100, rowSize = 50;
+	private JButton[][] solveButton;
 	private JButton oldClick;
 	private JButton reset;
 	private JLabel scoreJlabel;
+	private static ImageIcon[] images;
 	private int score = 0;
 	private static int countClick;
 	private final static int ROW = 6;
 	private final static int COL = 6;
-	public  JButton[][] getButton() {
-		return button;
+	public JButton[][] getSolveButton() {
+		return solveButton;
+	}
+	public void setSolveButton(JButton[][] solveButton) {
+		this.solveButton = solveButton;
 	}
 	public JLabel getScoreJlabel() {
 		return scoreJlabel;
 	}
 	public JButton getReset() {
 		return reset;
-	}
-	public void setButton(JButton[][] button, int i, int j , int value) {
-		button[i][j].setText("" + value);
-		this.button = button;
 	}
 	public static int getRow() {
 		return ROW;
@@ -43,28 +48,38 @@ public class SourceButton implements ActionListener{
 		}
 		return false;
 	}
-	public static JButton[][] init(JButton[][] matrix){
+	public static ImageIcon[] pikachuIcon(ImageIcon[] matrix){
+		for(int i = 1; i <= (13); ++i){
+			ImageIcon old = new ImageIcon(address + (i) + ".png");
+			Image cur = old.getImage().getScaledInstance(colSize, rowSize, Image.SCALE_SMOOTH);
+			matrix[i] = new ImageIcon(cur);
+		}
+		return matrix;
+	}
+	public static void init(JButton[][] solveButton){
 		for(int i = 0; i < ROW; ++i){
 			for(int j = 0; j < COL; ++j){
-				matrix[i][j].setText(".");
-				matrix[i][j].setBackground(Color.white);
+				solveButton[i][j].setText(".");
+				solveButton[i][j].setEnabled(true);
 			}
 		}
-		for(int value = 1; value <= (ROW * COL >> 2); ++value){
+		for(int value = 1; value <= (9); ++value){
 			for(int p = 0; p < 4; ++p){
 				Random numberRow = new Random();
 				Random numberCol = new Random();
 				int i = numberRow.nextInt(ROW);
 				int j = numberCol.nextInt(ROW);
-				if(matrix[i][j].getText().equals(".")){
-					matrix[i][j].setText("" + value);
+				if(solveButton[i][j].getText().equals(".")){
+					solveButton[i][j].setText("" + value);
+					solveButton[i][j].setIcon(images[13]);
 				}
 				else{
 					boolean okIndex = true;
 					for(int col = 0; col < ROW && okIndex; ++col){
 						for(int row = 0; row < ROW && okIndex; ++row){
-							if(indexNull(matrix[col][row].getText())){
-								matrix[col][row].setText("" + value);
+							if(indexNull(solveButton[col][row].getText())){
+								solveButton[col][row].setText("" + value);
+								solveButton[col][row].setIcon(images[13]);
 								okIndex = false;
 							}
 						}
@@ -72,23 +87,26 @@ public class SourceButton implements ActionListener{
 				}
 			}
 		}
-		
-		return matrix;
 	}
 	public SourceButton(){
-		button = new JButton[ROW][COL];
+		images = new ImageIcon[50];
+		solveButton = new JButton[ROW][COL];
 		for(int i = 0; i < ROW; ++i){
 			for(int j = 0; j < COL; ++j){
-				button[i][j] = new JButton();
-				button[i][j].setPreferredSize(new Dimension(50, 50));
+				solveButton[i][j] = new JButton();
+				solveButton[i][j].setPreferredSize(new Dimension(colSize, rowSize));
+				solveButton[i][j].setBackground(Color.white);
 			}
 		}
-		reset = new JButton("Reset");
+		reset = new JButton("Reset"); 
 		scoreJlabel = new JLabel("Score : 0");
-		button = init(button);
+		images = pikachuIcon(images);
+		init(solveButton);
+		scoreJlabel.setPreferredSize(new Dimension(colSize, rowSize));
+		reset.setPreferredSize(new Dimension(colSize, rowSize));
 		for(int i = 0; i < ROW; ++i){
 			for(int j = 0; j < COL; ++j){
-				button[i][j].addActionListener(this);
+				solveButton[i][j].addActionListener(this);
 			}
 		}
 		reset.addActionListener(this);
@@ -96,28 +114,31 @@ public class SourceButton implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		countClick = (countClick + 1) % 2;
-		JButton click = (JButton) e.getSource();
-		click.setVisible(false);
+		final JButton click = (JButton) e.getSource();
 		if(click.getText().equals(reset.getText())){
-			button = init(button);
+			init(solveButton);
 			score = 0;
 			countClick = 0;
-			scoreJlabel.setText("Score :" + score);
+			scoreJlabel.setText("Score : " + score);
 		}
 		else{
 			if(countClick == 1){
+				click.setIcon(images[Integer.parseInt(click.getText())]);
 				oldClick = click;
-				click.setBackground(Color.green);
 			}
 			else{
+				click.setIcon(images[Integer.parseInt(click.getText())]);
 				if(oldClick.getText().equals(click.getText())){
-					click.setBackground(Color.LIGHT_GRAY);
-					oldClick.setBackground(Color.LIGHT_GRAY);
 					score += 10;
 					scoreJlabel.setText("Score :" + score);
+					oldClick.setIcon(images[12]);
+					click.setIcon(images[12]);
+					oldClick.setEnabled(false);
+					click.setEnabled(false);
 				}
 				else{
-					oldClick.setBackground(Color.white);
+					oldClick.setIcon(images[13]);
+					click.setIcon(images[13]);
 				}
 			}
 		}
--- src/GamePikachu/SourcePanel.java
+++ src/GamePikachu/SourcePanel.java
@@ -1,13 +1,16 @@
 package GamePikachu;
 
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SourcePanel{
-	private JPanel panel, pl, pr, p1;
+	private JPanel panel, namePanel;
 	private SourceButton scButton;
 	public SourceButton getScButton() {
 		return scButton;
@@ -27,23 +30,29 @@ public class SourcePanel{
 	public SourcePanel(String user){
 		scButton = new SourceButton();
 		JLabel name = new JLabel("Name: " + user);
+		namePanel = new JPanel();
 		panel = new JPanel();
-		pl = new JPanel();
-		pr = new JPanel();
-		p1 = new JPanel();
-		pl.add(name);
-		p1.add(scButton.getScoreJlabel());
-		pr.add(p1);
-		panel.add(pl);
-		panel.add(pr);
+		panel.setLayout(new GridBagLayout());
+		namePanel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		int spacing = 10;
+		c.insets = new Insets(spacing, spacing, spacing, spacing);
 		for(int i = 0; i < SourceButton.getRow(); ++i){
-			final JPanel p = new JPanel();
+			c.gridy = i + 1;
 			for(int j = 0; j < SourceButton.getCol(); ++j){
-				p.add(scButton.getButton()[i][j]);
+				c.gridx = j;
+				panel.add(scButton.getSolveButton()[i][j], c);
 			}
-			panel.add(p);
 		}
-		panel.add(scButton.getReset());
-		panel.setLayout(new GridLayout(6, 2));
+		c.gridy = 0;
+		c.gridx = 0;
+		name.setPreferredSize(new Dimension(70, 50));
+		panel.add(name, c);
+		c.gridx = 5;
+		panel.add(scButton.getScoreJlabel(), c);
+		c.gridy = SourceButton.getRow() + 1;
+		c.gridx = 0;
+		panel.add(scButton.getReset(), c);
 	}
 }
